80295546578e8ee18b41cb0a3fcba811a245b377
G-Lumia <giorgiolumia@gmail.com>
2023-04-18 00:47:45 +0200

    header component created

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <HeaderComponent title="Yu-Gi Oh" />
+</template>
+  
+<script>
+    import HeaderComponent from './components/HeaderComponent.vue';
+    export default {
+    name: 'App',
+    components: {
+        HeaderComponent
+    }}
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class=" container bg-primary d-flex justify-content-center py-5 my-4">
+        <h1 class="text-light"> {{ title }} </h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    props: {
+        title: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
